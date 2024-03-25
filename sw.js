cddf98397d21d08cc40c930bcd153e43c0543a2f
Serge Simonov <sergee@gmail.com>
2024-03-24 18:29:45 -0600

    Added app.sj to the service worker

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@ const test = url => {
         return false;
     }
     const path = url.pathname;
-    return !path.match(/admin|sw.js|newton.html/) && !path.startsWith('/content');
+    return !path.match(/admin|sw.js|app.js|newton.html/) && !path.startsWith('/content');
 };
 
 const dir = async () => {
